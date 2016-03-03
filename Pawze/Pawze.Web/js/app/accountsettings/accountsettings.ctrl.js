8922a3909bb03281faccc6f1cf887b8c78b316c3
Taylor Smith <taylorsmith4546@gmail.com>
2016-03-02 17:19:40 -0800

    account settings step 1

--- Pawze/Pawze.Web/js/app/accountsettings/accountsettings.ctrl.js
+++ Pawze/Pawze.Web/js/app/accountsettings/accountsettings.ctrl.js
@@ -1,3 +1,18 @@
-﻿angular.module('app').controller('AccountSettingsController', function ($scope) {
+﻿angular.module('app').controller('AccountSettingsController', function ($scope, PawzeUserResource) {
+        //$scope.settings = {};
+
+        //$scope.save = function () {
+        //    AuthenticationService.registerCustomer($scope.registration).then(
+        //        function (response) {
+        //            alert("Registration Complete");
+        //            $timeout(function () {
+        //                location.replace('/#/login');
+        //            }, 2000);
+        //        },
+        //        function (error) {
+        //            alert("Failed To Register");
+        //        }
+        //    );
+        //};
 
 });
--- Pawze/Pawze.Web/js/app/app.ctrl.js
+++ Pawze/Pawze.Web/js/app/app.ctrl.js
@@ -1,3 +1,20 @@
-﻿angular.module('app').controller('AppController', function ($scope, localStorageService) {
-    $scope.user = localStorageService.get('user');
+﻿angular.module('app').controller('AppController', function ($scope, localStorageService, apiUrl, $http) {
+    
+
+    function activate() {
+        $http.get(apiUrl + '/pawzeuser/user')
+          .then(function (response) {
+              $scope.user = response.data;
+          })
+          .catch(function (err) {
+              alert('Failed to get the user');
+          });
+    };
+
+    activate();
+
+    $scope.logout = function () {
+        AuthenticationService.logout();
+        location.replace('#/home');
+    };
 });
